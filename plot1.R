65bf0b874c162cf61abdcbeea278ae74bf01fa5f
nicholaschris <nicholaschristopherpringle@gmail.com>
2015-01-11 00:16:10 +0100

    Fixed download

--- plot1.R
+++ plot1.R
@@ -1,9 +1,10 @@
 file_name <- "household_power_consumption.txt"
 if (!file.exists(file_name)){
   message("Downloading data")
-  download.file("https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip", file_name, method="wget")
-  unzip(filename)
+  download.file("https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip", "household_power_consumption.zip", method="wget")
+  unzip("household_power_consumption.zip")
 }
+
 
 DT <- fread(paste("grep ^[12]/2/2007", file_name), na.strings = c("?", ""))
 setnames(DT, colnames(fread(file_name, nrows=0)))
